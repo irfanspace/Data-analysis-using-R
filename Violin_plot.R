4cb54be044f2a8e8765cc8612b2d7f20d9c3e760
Irfan Khan <160655112+irfanspace@users.noreply.github.com>
2026-02-03 19:26:16 +0600

    Add files via upload

---
+++ Violin_plot.R
@@ -0,0 +1,41 @@
+#Violin Plot
+#package install
+install.packages(c("ggplot2", "gcookbook", "MASS", "dplyr"))
+library(ggplot2)
+
+library(gcookbook) # Load gcookbook for the heightweight data set
+
+# Create a base plot using the heightweight data set
+hw_p <- ggplot(heightweight, aes(x = sex, y = heightIn))
+
+hw_p +
+  geom_violin()
+#
+hw_p +
+  geom_violin() +
+  geom_boxplot(width = .1, fill = "black", outlier.colour = NA) +
+  stat_summary(fun.y = median, geom = "point", fill = "white", shape = 21, size = 2.5)
+
+hw_p +
+  geom_violin(trim = FALSE)
+
+# Scaled area proportional to number of observations
+hw_p +
+  geom_violin(scale = "count")
+
+#To change the amount of smoothing, use the adjust parameter. The default value is 1; 
+#use larger values for more smoothing and smaller values for less smoothing
+# More smoothing
+hw_p +
+  geom_violin(adjust = 2)
+
+# Less smoothing
+hw_p +
+  geom_violin(adjust = .5)
+
+
+
+
+
+
+
